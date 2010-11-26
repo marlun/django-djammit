1d8b4c82dcc07944093c126b4dfeea810cb93135
David Guaraglia <david@guaraglia.com>
2010-11-26 02:39:03 -0200

    added script type to script tag

--- djammit/utils.py
+++ djammit/utils.py
@@ -1,7 +1,7 @@
 def javascript_include_tag(sources):
     tags = []
     for source in sources:
-        tags.append('<script src="%s"></script>' % source)
+        tags.append('<script type="text/javascript" src="%s"></script>' % source)
     return "\n".join(tags)
 
 def remove_dups(paths):
